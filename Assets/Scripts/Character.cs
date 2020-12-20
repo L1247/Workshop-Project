648f9021a580fbce1be3e0df9db14569df2387db
rStar <starlove124760@gmail.com>
2020-12-20 17:58:00 +0800

    feat : add Character.SetMaxHp

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,6 +5,7 @@ namespace Workshop.Scripts
     public class Character
     {
         private int _currentHp = 100;
+        private int _maxHp;
 
         public int GetHp() => _currentHp;
 
@@ -19,12 +20,17 @@ namespace Workshop.Scripts
         {
             if (IsNegativeNumber(amount)) return;
             _currentHp += amount;
-            _currentHp =  Mathf.Min(100 , _currentHp);
+            _currentHp =  Mathf.Min(_maxHp , _currentHp);
         }
 
         private static bool IsNegativeNumber(int amount)
         {
             return amount < 0;
+        }
+
+        public void SetMaxHp(int amount)
+        {
+            _maxHp = amount;
         }
     }
 }
--- Assets/Scripts/Tests/HealthTests.cs
+++ Assets/Scripts/Tests/HealthTests.cs
@@ -30,9 +30,9 @@ namespace Tests
         }
 
         [Test]
-        [TestCase(7,93)]
-        [TestCase(-6,100)]
-        [TestCase(-20000,100)]
+        [TestCase(7 ,      93)]
+        [TestCase(-6 ,     100)]
+        [TestCase(-20000 , 100)]
         public void Hp_Decrease_When_Character_TakeDamage(int amount , int expected)
         {
             _character.TakeDamage(amount);
@@ -40,24 +40,26 @@ namespace Tests
         }
 
         [Test]
-        [TestCase(8 , 100)]
-        [TestCase(-8 , 100)]
-        public void Hp_Have_MaxValue_When_Character_Heal_Then_Hp_Increase(int amount , int expected)
+        [TestCase(8 ,  100,100)]
+        [TestCase(-8 , 100,100)]
+        [TestCase(19900 , 20000,20000)]
+        public void Hp_Have_MaxValue_When_Character_Heal_Then_Hp_Increase(
+            int amount , int expected , int maxHp)
         {
+            _character.SetMaxHp(maxHp);
             _character.Heal(amount);
             HpShouldBe(expected);
         }
 
         [Test]
-        [TestCase(100,   0)]
-        [TestCase(101, 0)]
-        [TestCase(200,  0)]
+        [TestCase(100 , 0)]
+        [TestCase(101 , 0)]
+        [TestCase(200 , 0)]
         public void HpHave_MinValue_Character_TakeDamage(int amount , int expected)
         {
             _character.TakeDamage(amount);
             HpShouldBe(expected);
         }
-
 
     #endregion
 
