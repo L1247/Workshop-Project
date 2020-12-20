eb85efbe9f53457fea5119167006dd6d2a608797
rStar <starlove124760@gmail.com>
2020-12-20 17:46:50 +0800

    feat : Extract method IsNegativeNumber

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,17 +10,21 @@ namespace Workshop.Scripts
 
         public void TakeDamage(int amount)
         {
-            if (amount < 0) return;
+            if (IsNegativeNumber(amount)) return;
             _currentHp -= amount;
             _currentHp =  Mathf.Max(0 , _currentHp);
-
         }
 
         public void Heal(int amount)
         {
-            if (amount < 0) return;
+            if (IsNegativeNumber(amount)) return;
             _currentHp += amount;
-            _currentHp=Mathf.Min(100 , _currentHp);
+            _currentHp =  Mathf.Min(100 , _currentHp);
+        }
+
+        private static bool IsNegativeNumber(int amount)
+        {
+            return amount < 0;
         }
     }
 }
